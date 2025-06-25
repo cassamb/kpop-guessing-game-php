8e46ed715172d9479ffe89eb42d8f858eb98bfc5
cassamb <rosecass99@gmail.com>
2025-06-25 13:33:25 -0400

    Completed Database Handler and Shuffled, Retrieved, and Set Name and URL arrays

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
+<?php
+    require_once "dbh.php"; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kpop Guessing Game: PHP</title>
+    
+    <script>
+        // Retrieving shuffled names and urls from PHP file
+        var names = <?php echo json_encode($names)?>;
+        var urls = <?php echo json_encode($urls)?>;
+        
+        document.write(names);
+        document.write(urls);
+    </script>
 </head>
 <body>
     
